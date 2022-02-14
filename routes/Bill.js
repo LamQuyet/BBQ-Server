583d13379bef33deb7fbc3e87446db03472ac8b7
quyetlv <quyetlv@caerux.com>
2022-02-14 11:17:09 +0700

    uninstall nodemon

--- routes/Bill.js
+++ routes/Bill.js
@@ -8,7 +8,7 @@ router.use('/history', billController.History)
 router.use('/newbills', billController.getNewBills)
 router.use('/billscanceled', billController.getCanceledBills)
 router.post('/accept', billController.Update)
-router.use('/day', billController.BillsToday)
+router.post('/day', billController.BillsToday)
 router.post('/month', billController.ThisMonthBills)
 router.post('/year', billController.BillsYear)
 
